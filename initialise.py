6514b25ef33fe0b0cf11c5f36863b1a52f10390d
gokul <gokulap@student.tce.edu>
2021-09-25 22:25:01 +0530

    added a new db table to save target queue

--- initialise.py
+++ initialise.py
@@ -17,6 +17,7 @@ bot.send_message(CHAT_ID, "Test Message from Reconator !")
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 cur = conn.cursor()
 
+# to save the result of recon on target
 cur.execute('''
 
 create table output (
@@ -27,7 +28,20 @@ result varchar(10000000)
 );
 
 ''')
-
 conn.commit()
+
+# to save targets in queue
+cur.execute('''
+
+create table queue (
+
+id varchar(5),
+target varchar(50)
+
+);
+
+''')
+conn.commit()
+
 cur.close()
 conn.close()
