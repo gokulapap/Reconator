a0eeb258e38ea258b378a44b835314d9c6e719f7
gokul <gokulap@student.tce.edu>
2021-09-27 20:46:54 +0530

    added table in cron

--- cron.py
+++ cron.py
@@ -1,20 +1,31 @@
-from time import sleep
 import os
+from time import sleep
+import psycopg2
+import base64
 
-#checks the domains.txt every 60 secs
-while True:
-  a = open('domains.txt', 'r')
-  b = a.readlines()
-  a.close()
-  if len(b) == 0:
-    pass
-  else:
-    url = b[0]
-    b.pop(0)
-    f = open('domains.txt', 'w')
-    for i in b:
-      f.write(i)
-    f.close()
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+cur = conn.cursor()
+
+
+#checks the db queue table every 60 secs
+While True:
+ try:
+   cur.execute("select * from queue limit 1;")
+   t = cur.fetchall()
+   t = t[0]
+   id = t[0]
+   url = t[1]
+
+
+   conn.commit()
+
+cur.close()
+conn.close()
+
+
+
 
     #sends to run.py
     os.system('python run.py {}'.format(url))
