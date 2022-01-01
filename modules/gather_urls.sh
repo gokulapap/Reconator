a98d5380b7568d6f6429be87ef6e328f1795983a
gokulap <gokulap.tce@gmail.com>
2022-01-01 19:59:13 +0530

    removed thread numbers

--- modules/gather_urls.sh
+++ modules/gather_urls.sh
@@ -6,7 +6,7 @@ echo "16) GATHERING ALL URLS" >> /app/results/$url-output.txt
 printf "\n\n" >> /app/results/$url-output.txt
 
 /app/modules/binaries/gau $url | egrep -v '(.pdf|.css|.png|.jpeg|.jpg|.svg|.gif|.wolf)' | tee -a /app/urls
-/app/modules/binaries/gospider -t 150 -s http://$url -d 4 | grep "\[url\]" | cut -d " " -f 5 | egrep -v '(.pdf|.css|.png|.jpeg|.jpg|.svg|.gif|.wolf)' | tee -a /app/urls
+/app/modules/binaries/gospider -t 80 -s http://$url -d 4 | grep "\[url\]" | cut -d " " -f 5 | egrep -v '(.pdf|.css|.png|.jpeg|.jpg|.svg|.gif|.wolf)' | tee -a /app/urls
 /app/modules/binaries/hakrawler -plain -url http://$url -depth 4 | egrep -v '(.pdf|.css|.png|.jpeg|.jpg|.svg|.gif|.wolf)' | tee -a /app/urls
 
 cat /app/urls | qsreplace -a | tee /app/final_urls
