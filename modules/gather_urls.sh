05a7551fe2404bb0ad316b5918b42a4a95d25e48
GOKUL A.P <gokulap.tce@gmail.com>
2022-03-01 11:57:40 +0530

    fixed merging of two results

--- modules/gather_urls.sh
+++ modules/gather_urls.sh
@@ -6,10 +6,10 @@ url=$1
 echo "16) GATHERING ALL URLS" >> /app/results/$url-output.txt
 printf "\n\n" >> /app/results/$url-output.txt
 
-/app/modules/binaries/gau $url | egrep -v '(.pdf|.css|.png|.jpeg|.jpg|.svg|.gif|.wolf)' | tee -a /app/urls
-/app/modules/binaries/gospider -t 20 -s http://$url -d 3 | grep "\[url\]" | cut -d " " -f 5 | egrep -v '(.pdf|.css|.png|.jpeg|.jpg|.svg|.gif|.wolf)' | tee -a /app/urls
+/app/modules/binaries/gau $url | egrep -v '(.pdf|.css|.png|.jpeg|.jpg|.svg|.gif|.wolf)' | tee -a /app/$url-urls.txt
+/app/modules/binaries/gospider -t 20 -s http://$url -d 3 | grep "\[url\]" | cut -d " " -f 5 | egrep -v '(.pdf|.css|.png|.jpeg|.jpg|.svg|.gif|.wolf)' | tee -a /app/$url-urls.txt
 
-cat /app/urls | /app/modules/binaries/qsreplace -a | tee /app/results/$url-gau.txt
+cat /app/$url-urls.txt | /app/modules/binaries/qsreplace -a | tee /app/results/$url-gau.txt
 
 printf "Gathered all urls for the $url successfully !\n" >> /app/results/$url-output.txt
 printf "Goto /gau/{url} to view all the gathered urls | Download the file for your future recon" >> /app/results/$url-output.txt
