8c1d0fd0509d61d20a3e7aaccf1fc7737ad5be4b
Pugalarasan <75373225+0xlittleboy@users.noreply.github.com>
2022-10-21 21:27:25 +0530

    Update subtake.sh

--- modules/subtake.sh
+++ modules/subtake.sh
@@ -6,7 +6,7 @@ url=$1
 echo "10) SUBDOMAINS TAKEOVER CHECK" >> /app/results/$url-output.txt
 printf "\n\n" >> /app/results/$url-output.txt
 
-/app/modules/binaries/subzy -concurrency 90 -timeout 5 -hide_fails -targets /app/$url-subs | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g" | tee -a /app/results/$url-output.txt
+/app/modules/binaries/subzy -concurrency 90 -timeout 20 -hide_fails -targets /app/$url-subs | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g" | tee -a /app/results/$url-output.txt
 
 printf "\n\n\n" >> /app/results/$url-output.txt
 printf "##########################################################################################\n" >> /app/results/$url-output.txt
